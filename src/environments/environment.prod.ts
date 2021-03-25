7249aecc6f412d99e5d9921b9ae92ef551614f87
Ali Toghiani <ali.toghiani.dev@gmail.com>
2021-03-25 16:33:52 +0430

    generated deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyChBQWVSYQVAiUBWYSNBlUBa6O40YMHCUc",
+    authDomain: "omid-shelter.firebaseapp.com",
+    databaseURL: "https://omid-shelter.firebaseio.com",
+    projectId: "omid-shelter",
+    storageBucket: "omid-shelter.appspot.com",
+    messagingSenderId: "372885562625",
+
+  }
 };
